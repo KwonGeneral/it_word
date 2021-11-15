2d6e3dda1f991749321b3f75e4eea711a5f8d605
Kwonputer <thtm9020@gmail.com>
2021-11-15 17:50:45 +0900

    first commit

---
+++ app/src/main/java/com/kwon/it_word/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kwon.it_word
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
